3cbbb7c2e6799dcc98e08f7ac454618474c33665
Serkan Kayser <serkan@Serkans-MacBook-Pro.local>
2025-10-03 13:43:34 +0300

    changes3 in tsconfig

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
 import { withPayload } from '@payloadcms/next/withPayload'
 
 import redirects from './redirects.js'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
@@ -17,6 +12,7 @@ const nextConfig = {
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
         const url = new URL(item)
+
         return {
           hostname: url.hostname,
           protocol: url.protocol.replace(':', ''),
@@ -30,8 +26,6 @@ const nextConfig = {
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
       '.mjs': ['.mts', '.mjs'],
     }
-
-    webpackConfig.resolve.alias['@'] = path.resolve(__dirname, 'src')
 
     return webpackConfig
   },
