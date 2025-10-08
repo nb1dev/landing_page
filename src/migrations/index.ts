080fb0b70e752625a28203f796783d692d104cb7
Serkan Kayser <serkan@Serkans-MacBook-Pro.local>
2025-10-08 13:34:44 +0300

    removed migration files

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20251007_144937_init from './20251007_144937_init';
-
-export const migrations = [
-  {
-    up: migration_20251007_144937_init.up,
-    down: migration_20251007_144937_init.down,
-    name: '20251007_144937_init'
-  },
-];
