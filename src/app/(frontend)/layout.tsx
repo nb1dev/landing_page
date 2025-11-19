1b7229102101e7bec4b53107e7062267af6ef3b8
nb1dev <dev@nb1.com>
2025-11-19 15:49:16 +0200

    added chat and privacy scripts

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -6,8 +6,6 @@ import { GeistSans } from 'geist/font/sans'
 import React from 'react'
 
 import { AdminBar } from '@/components/AdminBar'
-// import { Footer } from '@/Footer/Component'
-// import { Header } from '@/Header/Component'
 import { Providers } from '@/providers'
 import { InitTheme } from '@/providers/Theme/InitTheme'
 import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
@@ -19,6 +17,8 @@ import '@fontsource/inter/400.css'
 import '@fontsource/inter/500.css'
 import '@fontsource/instrument-sans/500.css'
 import '@fontsource/instrument-sans/400.css'
+import Script from 'next/script'
+import { KetchScriptLoader } from './KetchScriptLoader' // 👈 add this
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const { isEnabled } = await draftMode()
@@ -27,20 +27,25 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <html className={cn(GeistSans.variable, GeistMono.variable)} lang="en" suppressHydrationWarning>
       <head>
         <InitTheme />
+        <Script src="https://cdn.botpress.cloud/webchat/v3.4/inject.js" />
+        <Script
+          src="https://files.bpcontent.cloud/2025/11/19/08/20251119085549-S157I4GF.js"
+          defer
+        />
         <link href="/favicon-1.ico" rel="icon" sizes="32x32" />
         <link href="/favicon-1.svg" rel="icon" type="image/svg+xml" />
       </head>
       <body>
+        {/* Load Ketch only on the client after hydration */}
+        <KetchScriptLoader />
+
         <Providers>
           <AdminBar
             adminBarProps={{
               preview: isEnabled,
             }}
           />
-
-          {/* <Header /> */}
           {children}
-          {/* <Footer /> */}
         </Providers>
       </body>
     </html>
