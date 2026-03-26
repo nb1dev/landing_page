e63d3cff47c04bcd5a1421cee96b4707ab7c146b
nb1dev <dev@nb1.com>
2026-03-26 11:10:45 +0200

    added meta pixel script

--- src/app/(frontend)/[locale]/layout.tsx
+++ src/app/(frontend)/[locale]/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import type { Metadata } from 'next'
 
 import { cn } from '@/utilities/ui'
@@ -78,7 +79,7 @@ export default async function RootLayout({
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=!0;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-KQBDCQ9B');
           `}
@@ -105,12 +106,37 @@ export default async function RootLayout({
           `}
         </Script>
 
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1392496046228330');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
         <link href="/favicon-1.ico" rel="icon" sizes="32x32" />
         <link href="/favicon-1.svg" rel="icon" type="image/svg+xml" />
         <JsonLd data={organizationJsonLd} />
       </head>
 
       <body>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1392496046228330&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
         {/* Google Tag Manager (noscript - DISABLED because using ConditionalGoogleTagManager) */}
         {/*
         <noscript>
